5ebac8570c332a417bd3b64ea75323a8702fb4c5
DarkZ <93233687+MirrorDevelopers@users.noreply.github.com>
2021-12-14 21:40:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone github.com/TeamInfinityGo/InfinityMovieBot 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /InfinityMovieBot 
 fi
-cd /EvaMaria
+cd /InfinityMovieBot 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
